02f3b6154714e34715178cafb39613bf90b9d629
Luis Aza <luismiac93@outlook.com>
2017-10-15 11:56:52 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appName: 'platzimusic',
+  apiKey: '3eaa828026096f2529593b39cbeb66d8',
+  secret: '6814b00d8b9c93c737988dcda9db8731',
+  registeredTo: 'lazacaicedo'
+}
+
+// Application name 	platzimusic
+// API key 	3eaa828026096f2529593b39cbeb66d8
+// Shared secret 	6814b00d8b9c93c737988dcda9db8731
+// Registered to 	lazacaicedo
